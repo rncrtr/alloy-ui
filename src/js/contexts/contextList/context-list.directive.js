0f80ccc473da838510f62f29b9595747809be4e2
Ryan Carter <ryan.carter@charter.com>
2016-03-04 17:16:54 -0700

    added dynamic actions and hidden fields to datatables

--- src/js/contexts/contextList/context-list.directive.js
+++ src/js/contexts/contextList/context-list.directive.js
@@ -10,21 +10,37 @@ function ContextListDirective() {
         link: link
     }
 
-    function link($scope, $elem, $attrs) {
+    function link(scope, $elem, $attrs) {
         // set config
-        var dt = {};
-        dt.rest_url = '';
-        dt.rest_method = '';
-        dt.rest_params = '';
-        dt.currPage = 1;
-        dt.pageSize = 25;
-        dt.pageSets = ['10','25','50','100','500','1000'];
-        dt.totalItems = 100;
-        dt.sortReverse = false;
-        dt.search = '';
-        dt.fieldlabels = ['Field 1','Field 2','Field 3'];
-        dt.sortType = 'field1';
-        $scope.contexts = dt;
+        var viewData = {};
+        viewData.rest_url = '';
+        viewData.rest_method = 'get';
+        viewData.rest_params = '';
+        viewData.currPage = 1;
+        viewData.pageSize = 25;
+        viewData.pageSets = [
+            {'name':'10','value':10},
+            {'name':'25','value':25},
+            {'name':'50','value':50},
+            {'name':'100','value':100},
+            {'name':'500','value':500},
+            {'name':'1000','value':1000}
+        ];
+        viewData.totalItems = 39;
+        viewData.lastPage = Math.round(viewData.totalItems / viewData.pageSize);
+        viewData.sortReverse = false;
+        viewData.search = '';
+        viewData.fieldLabels = ['Field 1'];
+        viewData.hideFields = ['id'];
+        viewData.sortType = 'id';
+
+        // array of action objects label, class, url to for
+        viewData.actions = [
+            {'label':'View','class':'btn-primary','url':'/context/:id'}
+            //{'label':'Edit','class':'btn-success','url':'/context/:id/edit'},
+            //{'label':'Delete','class':'btn-danger','url':'/context/:id/delete'}
+        ];
+        scope.contexts = viewData;
     }
 
 }
--- src/js/gridTable/grid-table.directive.js
+++ src/js/gridTable/grid-table.directive.js
@@ -1,6 +1,7 @@
 'use strict';
 gridTable.$inject = ['dataService'];
 function gridTable(dataService) {
+  
   var directive = {
     scope:{
       viewData: '=view'
@@ -11,14 +12,40 @@ function gridTable(dataService) {
   };
   return directive;
 
-  function link(scope, element, attrs) {
-    console.log('grid table I choose you!');
+  function link(scope, element, attrs){
+    // init nicer vars for local consumption and reuse
     var url = scope.viewData.rest_url;
     var method = scope.viewData.rest_method.toUpperCase();
     var params = scope.viewData.rest_params;
-    dataService.getRest(url, method, params).then(function(result){
-      $scope.results = result;
-    });
+
+    getData();
+    scope.tellMeAboutIt = function(id, url){
+      console.log(id, url);
+      if(url.indexOf(':id')){
+        url = url.replace(/\:id/ig,id);
+      }
+      console.log(url);
+      location.href = url;
+    }
+
+    // call data on the service
+    function getData(){
+      dataService.getRest(url, method, params).then(function(result){
+        console.log(result);
+        scope.results = result;
+      });
+    }
+
+    // requery data when the pageSize is selected
+    scope.pageSizeChanged = function() {
+      scope.viewData.lastPage = Math.round(scope.viewData.totalItems / scope.viewData.pageSize);
+      return getData();
+    };
+
+    // requery data when the currPage changes
+    scope.$watch('viewData.currPage', (function() {
+      getData();
+    }));
   }
 }
 
